567a1a92dde1155011fecd87ac25278036550c8f
Bobby Ayers <rsheldon3ayers@gmail.com>
2017-06-24 15:36:12 -0500

    initial commit

---
+++ src/Recursion.java
@@ -0,0 +1,19 @@
+/**
+ * Created by Robert Ayers on 6/23/17.
+ */
+public class Recursion
+{
+    public static long factorial(long number)
+    {
+        if (number <= 1)
+            return 1;
+        else
+            return number * factorial(number - 1);
+    }
+
+    public static void main(String[] args)
+    {
+        for (int counter = 0; counter <= 21; counter++)
+            System.out.printf("%d! = %d%n", counter, factorial(counter));
+    }
+}
